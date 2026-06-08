63d280497c7c8b08ef16b7328a51ac96e156c9ba
Ori Ariel <74496673+oriariel@users.noreply.github.com>
2026-06-08 17:05:40 +0300

    Add files via upload

---
+++ Web_Automation_n_Data_Pipeline/config/config/config.py
@@ -0,0 +1,12 @@
+# config/config.py
+import os
+
+# Browser Settings
+CHROME_VERSION = 148
+IMPLICIT_TIMEOUT = 10
+
+# Target Data Endpoint (Using an open e-commerce/scraping sandbox for development)
+TARGET_URL = "https://www.scrapingcourse.com/ecommerce/"
+
+# Alert Settings (Can be wired to Discord/Slack later)
+ENABLE_ALERTS = False
